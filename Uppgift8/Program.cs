6022ab1a6be07fce2e8b6ba7d36d49d72098a6fc
jaste_06@edu.sollentuna.se <jaste_06@edu.sollentuna.se>
2023-12-01 12:31:11 +0100

    ny

--- Uppgift8/Program.cs
+++ Uppgift8/Program.cs
@@ -11,23 +11,34 @@ namespace Uppgift8
         static void Main(string[] args)
         {
             int antal = int.Parse(Console.ReadLine());
-            int[] pris = new int [antal];
+            int[] pris = new int[antal];
             int minstaPris = 0;
             int gratisBöcker = antal / 3;
-            for (int i = 0; i < antal; i++) 
+            int borttagnaBöcker = 0;
+            for (int i = 0; i < antal; i++)
             {
                 pris[i] = int.Parse(Console.ReadLine());
             }
-            
-            Array.Sort(pris);
 
-            for (int j=(gratisBöcker);j<antal; j++)
+            //Sorterar fältet så det minsta talet kommer först
+            Array.Sort(pris);
+            //Sorterar fältet så det största talet kommer först
+            Array.Reverse(pris);
+            for (int j = 0; j < antal; j++)
             {
                 minstaPris += pris[j];
+
+            
+                    if ((j+1) % 3 == 0 && borttagnaBöcker<gratisBöcker)
+                    {
+                        minstaPris -= pris[j];
+                        borttagnaBöcker++;
+                    }
             }
 
             Console.WriteLine(minstaPris.ToString());
-            Console.ReadKey();  
+
+            Console.ReadKey();
         }
     }
 }
